05e043c7bb3ca64660d9cc6d5279090aff3528f5
BernardDumenu <152184847+BernardDumenu@users.noreply.github.com>
2024-01-07 09:55:30 -0500

    Update textgame.sh

--- textgame.sh
+++ textgame.sh
@@ -2,3 +2,11 @@ echo "1. Male"
 echo "2. Female"
 echo "3. Non-Binary"
 echo "4. Quit"
+read user_character
+echo "You are $user_character singer in an nightclub"
+echo "Suddenly, gun shots were heard outside."
+echo "What would you do?"
+echo "1. Go outside"
+echo "2. Remember calm inside"
+echo "3. Call the cops"
+read user_action
